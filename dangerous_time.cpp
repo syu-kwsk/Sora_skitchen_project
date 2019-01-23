d386391028c12b54dbfeb148f486776e1ae9baf4
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-01-23 16:23:52 +0900

    Add Mode
    モード選択を追加しました

--- dangerous_time.cpp
+++ dangerous_time.cpp
@@ -2,7 +2,10 @@
 #include <time.h>
 #include "mbed.h"
 
-DigitalOut led[4] = {(LED1), (LED2), (LED3), (LED4)};
+#define RESET 99
+
+
+DigitalOut led[5] = {(p9), (LED1), (LED2), (LED3), (LED4)};
 DigitalOut led2(LED2);
 DigitalOut led3(LED3);
 DigitalOut led4(LED4);
@@ -14,25 +17,25 @@ AnalogIn mysen(p20);
 Timer t;
 
 void light(int light){
-  led[light - 1] = 1;
+  led[light] = 1;
   wait(1);
-  led[light - 1] = 0;
+  led[light] = 0;
 }
 
 
 void pi_sound(){
-#define PI 1.0/960.000 
-#define WRITE 0.5
-#define WAIT_SIREN 0.65    
+  const double PI = 1.0/960.000; 
+  const double WRITE = 0.5;
+  const double WAIT_SIREN = 0.65;    
 
   mysp.period(PI);
   mysp.write(WRITE);
   wait(WAIT_SIREN);
 }
 void po_sound(){
-#define PO 1.0/770.000
-#define WRITE 0.5
-#define WAIT_SIREN 0.65    
+  const double PO = 1.0/770.000;
+  const double WRITE = 0.5;
+  const double WAIT_SIREN = 0.65;    
    
   mysp.period(PO);
   mysp.write(WRITE);
@@ -40,8 +43,8 @@ void po_sound(){
 }
 
 void famima_sound(){
-#define WRITE 0.5  
-#define WAIT 5.0/14.0
+  const double WRITE = 0.5;  
+  const double WAIT = 5.0/14.0;
   
   mysp.period(1.0/739.989); //ファ＃
   mysp.write(WRITE);
@@ -95,9 +98,9 @@ void famima_sound(){
 }
 
 void meter(int lv){
-#define RESET 99
-  if(lv < 11){
-    for(int i = 0;i < lv;i++){
+
+  if(lv < 10){
+    for(int i = 0;i <= lv;i++){
       a[i] = 1;
     }
   }
@@ -109,8 +112,7 @@ void meter(int lv){
 }
 
 void wave(int lv){
-#define WAVETIME 0.02
-#define RESET 99
+  const double WAVETIME = 0.02;
   if(lv < 11){
     for(int i = 0;i < lv;i++){
       a[i] = 0;
@@ -129,6 +131,8 @@ void exit(){
   t.reset();
   led[1] = 0;
   led[2] = 0;
+  led[3] = 0;
+  led[4] = 0;
   a[0] = 0;
   a[1] = 0;
   a[2] = 0;
@@ -139,73 +143,134 @@ void exit(){
   a[7] = 0;
   a[8] = 0;
   a[9] = 0;  
+  mysp.write(0);
 }
+
 
 
 int main(void){
+  enum Mode{Init, Alert, Famima}; /*Init   = 初期モード
+                                    Alert  = 防犯モード  
+                                    Famima = コンビニモード*/
+   
+  enum Mode mode;//モードの種類
+  
   while(true){
-    static int k = 0;
+    static int power = 0;//電源の状態
+    static int k = 0;//各モードの段階
+    
     if (mysw1 == 1){
-      light(1);
-      if (k == 0){
-        light(2);
-        k++;
+      light(1); //スイッチ１を押されて、LED1点灯
+      if (power == 0){
+        power++;//電源をいれて
+        k++;//段階を開始して
+        light(2);//LED２点灯
       }
-      else if (k != 0){
-        k = 0;
-        light(3);
-        exit();              
+      
+      else if (power != 0){
+        k = 0;//段階をリセットして
+        power = 0;//電源をリセットして
+        mode = Init;//モードを初期モードにして
+        exit();//すべての出力を切り
+        light(3);//LED3点灯             
       }
+    }
+    
+    
+    /*電源ONのとき*/
+    if(power == 1){ 
+    
+      /*モード選択*/
+      if(mysw2 == 1){
+        k = 1;//段階を開始し直して
+        exit();//すべての出力を切ってから
+        mode = mode == Alert ? Famima : Alert;//モードを決定
+        light(4);//LED４点灯
+      } 
     }
  
  
-    if(k == 1){
-      if(mysen >=3.0/3.3){
-        k++;
-        t.reset();
-        t.start();
-        light(4);
-      }
-      else {
-        meter(RESET);
-      }
-    }
-
-    else if (k == 2){
-      if(mysen >= 3.0/3.3){
-        led[1] = 1;
-      }
-      if(mysen < 3.0/3.3){
-        led[1] = 0;
-        t.reset();
-        k--;
-      }  
-
-      for(int i = 0; i < 10; i++){
-        if (t > i && t <= i + 1){
-          meter(i);
+ 
+    /*防犯モードが選択中のとき*/
+    if(mode == Alert){
+      
+      /*センサー検出段階*/
+      if(k == 1){
+        if(mysen >=3.0/3.3){
+          t.reset();//警戒時間をリセットして
+          t.start();//警戒時間を計測して
+          k++;//次の段階へ
+          light(1);//LED１点灯
+        }
+        else {
+          meter(RESET);//検出していないなら、メーターの出力をリセット
         }
       }
-      if(t > 10 && t <= 11){
-        t.reset();
-        meter(RESET);
-        k++;
-      }
-    }
-    else if(k == 3){
-        
-      for(int i = 0;i < 10;i++){
-        wave(i);
-      }
-      pi_sound();
-      for(int i = 8;i >= 0;i--){
-        wave(i);
-        meter(99);
-      }   
-      po_sound();
-    }
 
+      /*危険度計測段階*/
+      else if (k == 2){
+        if(mysen >= 3.0/3.3){
+          led[1] = 1;//検出中はLED１点灯
+        }
+        if(mysen < 3.0/3.3){
+          led[1] = 0;//検出外はLED１消灯して
+          t.reset();//警戒時間をリセットして
+          k--;//検出段階に戻る
+        }  
+
+        for(int i = 0; i < 10; i++){
+          if (t > i && t <= i + 1){
+            meter(i);//１秒間ごとにメーターを上昇
+          }
+        }
+        if(t > 10 && t <= 11){
+          t.reset();//１０秒間を超えたら、警戒時間をリセットして
+          meter(RESET);//メーターもリセットして
+          k++;//次の段階へ
+        }
+      }
+      
+      /*警戒態勢段階*/
+      else if(k == 3){
+        
+        for(int i = 0;i < 10;i++){
+          wave(i);
+        }
+        pi_sound();
+        for(int i = 8;i >= 0;i--){
+          wave(i);
+          meter(RESET);
+        }   
+        po_sound();
+      }
+      
+    }//警戒モード終わり
     
-  
-  }
-}
+    
+    /*コンビニモード選択中のとき*/
+    if(mode == Famima){
+      
+      /*検出段階*/
+      if(k == 1){
+        if(mysen >= 3.0/3.3){
+          k++;//客が来たら、次の段階へ
+          light(1);//LED１点灯
+        }    
+      }
+          
+      /*来店段階*/    
+      else if(k == 2){
+      
+        if(mysen >= 3.0/3.3){
+          famima_sound();//来店BGMを流す
+        }
+      
+        if(mysen < 3.0/3.3){
+          k--;//いなくなったら、検出段階に戻る
+        }
+      }
+      
+    }//コンビニモード終わり
+ 
+  }//while終わり
+}//main終わり
